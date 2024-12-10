cadde874ed34dc4925e49777eafd9a31ff4da4ad
Joshua Portway <josh@stain.org>
2024-12-10 02:45:12 +0000

    cleaner

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -1,90 +1,114 @@
-import Day9.DiskChunk.End
-import Day9.DiskChunk.FileChunk
-import Day9.DiskChunk.FreeSpace
 import os.*
+
 object Day9 {
 
-  enum DiskChunk {
+  type ID = Int
 
-    case FileChunk(id: Int, length: Int)
-    case FreeSpace(length: Int)
-    case End
+  trait DiskChunk {
+
+    def getId: Option[ID]
+    def span: Int
+    def isFile: Boolean
+    def isFree: Boolean
 
   }
-  def parse(in: String): Seq[DiskChunk] = {
+
+  case class FileChunk(id: ID, span: Int) extends DiskChunk {
+
+    def isFile: Boolean = true
+    def isFree: Boolean = false
+
+    def sameFileAs(other: DiskChunk): Boolean = {
+      other match {
+        case o: FileChunk => o.id == this.id
+        case _            => false
+      }
+    }
+
+    override def getId: Option[ID] = Some(id)
+
+  }
+
+  case class FreeSpace(span: Int) extends DiskChunk {
+
+    def getId           = None
+    def isFile: Boolean = false
+    def isFree: Boolean = true
+
+  }
+
+  def parse(in: String): List[DiskChunk] = {
     val inStream = (in + "0").grouped(2).zipWithIndex.toList
-    println(inStream)
     val mappedStream =
-      inStream.flatMap((str, id) => List(FileChunk(id, str(0) - '0'), FreeSpace(str(1) - '0'))).toVector
-    val layout: Seq[DiskChunk] = mappedStream.collect {
-      case a: FileChunk                 => a
-      case s: FreeSpace if s.length > 0 => s
-    } // remove zero length gaps
+      inStream.flatMap((str, id) => List(FileChunk(id, str(0) - '0'), FreeSpace(str(1) - '0')))
+    val layout = mappedStream.filter(_.span > 0) // remove zero length gaps
     layout
   }
 
-  def fillGap(
-      spaceToFill: FreeSpace,
-      file: FileChunk,
-      queue: Seq[DiskChunk],
-      supply: Seq[FileChunk]
-  ): (FileChunk, DiskChunk, FileChunk, Seq[DiskChunk], Seq[FileChunk]) = {
-    val filled         = Math.min(spaceToFill.length, file.length)
-    val remainingFile  = file.length - filled
-    val remainingSpace = spaceToFill.length - filled
-    val (newFiller, newSupply) =
-      if remainingFile > 0 then {
-        val x: (FileChunk, Seq[DiskChunk.FileChunk]) = (FileChunk(file.id, remainingFile), supply) // ??? compiler bug - not finding type correctly???
-        x
-      } else (supply.head, supply.tail)
-    val (newHead, newQueue) =
-      if remainingSpace > 0 then (FreeSpace(remainingSpace), queue) else (queue.head, queue.tail)
-
-    (FileChunk(file.id, filled), newHead, newFiller, newQueue, newSupply)
+  def frag(layout: List[DiskChunk]): List[DiskChunk] = {
+    val backwards = layout.reverse.collect { case a: FileChunk => a }
+    List.unfold(layout, backwards) { case (remainder: List[DiskChunk], supply: List[FileChunk]) =>
+      remainder match {
+        case Nil => None // nothing left to work with
+        case next :: pending =>
+          next match {
+            case f: FileChunk => Some((f, (pending, supply)))
+            case spc: FreeSpace =>
+              val spanToFill = spc.span
+              val filler     = supply.head
+              val filledSpan = Math.min(spanToFill, filler.span)
+              val fill       = filler.copy(span = filledSpan)
+              val newSupply = if (filledSpan < filler.span) { // still some left
+                (filler.copy(span = filler.span - filledSpan)) :: supply.tail
+              } else {
+                supply.tail
+              }
+              val newRemainder = if (filledSpan < spanToFill) {
+                spc.copy(span = spanToFill - filledSpan) :: pending
+              } else {
+                if (newSupply.head.sameFileAs(pending.head)) { // we're done
+                  List(newSupply.head)                         // just have to copy any remaining from the current file
+                } else {
+                  pending
+                }
+              }
+              Some((fill, (newRemainder, newSupply)))
+          }
+      }
+    }
   }
 
-  def frag(layout: Seq[DiskChunk]): Seq[FileChunk] = {
-    val reversed = layout.reverse.collect { case a: FileChunk => a }
+  def defrag(layout: Vector[DiskChunk]): Vector[DiskChunk] = ???
 
-    // the state is currentChunk,currentFiller,remainingChuncks,remainingFiller
-    val compressed = List.unfold((layout.head, reversed.head, layout.tail, reversed.tail)) {
-      case (currentHead, currentFiller, queue, supply) =>
-        currentHead match {
-          case End => None
-          case s: FreeSpace =>
-            val (f, newHead, newFiller, newQueue, newSupply) = fillGap(s, currentFiller, queue, supply)
-            Some(f, (newHead, newFiller, newQueue, newSupply))
-          case f: FileChunk if f.id < currentFiller.id => Some(f, (queue.head, currentFiller, queue.tail, supply))
-          case f: FileChunk                            => Some(currentFiller, (End, f, queue, supply))
-        }
-    }
-    compressed
+  /** find the first free space that can fit the given size */
+  def findFirstFree(layout: Seq[DiskChunk], len: Int): Unit = {
+    val pos             = layout.indexWhere(x => x.isInstanceOf[FreeSpace] && x.span >= len)
+    val (before, after) = layout.splitAt(pos)
+
+  }
+
+  /** produces a lazily computed sequence of blocks with the id of each one */
+  def blocks(input: Seq[DiskChunk]): Iterable[Option[ID]] =
+    input.view.flatMap(chunk => Seq.fill(chunk.span)(chunk.getId))
+
+  def checksum(input: Seq[DiskChunk]): Long = blocks(input).zipWithIndex.foldLeft(0L) { case (acc, (n, idx)) =>
+    acc + (n.getOrElse(0) * idx)
   }
 
   def viz(layout: Seq[DiskChunk]): String = {
-    layout.map {
-      case f: FileChunk => f.id.toString.repeat(f.length)
-      case s: FreeSpace => ".".repeat(s.length)
+    blocks(layout).map {
+      case None     => "."
+      case Some(id) => id.toString
     }.mkString
-  }
-
-  def checksum(input: Seq[DiskChunk]): Long = {
-    val (result, _) = input.foldLeft((0L, 0)) { case ((acc, inx), chunk) =>
-      chunk match {
-        case FreeSpace(len) => (acc, inx + len)
-        case End            => ???
-        case FileChunk(id, len) =>
-          val sum = (inx until inx + len).foldLeft(0L) { case (ac2, ix2) => ac2 + (id * ix2) }
-          (acc + sum, inx + len)
-      }
-    }
-    result
   }
 
   @main
   def Day9main(): Unit = {
     val testIn     = parse(os.read(os.pwd / "input" / "Day9test.txt"))
     val testResult = frag(testIn)
+    println(testIn)
+    println(viz(testIn))
+    println(viz(testResult))
     assert(viz(testResult) == "0099811188827773336446555566")
     val chk = checksum(testResult)
     assert(chk == 1928)
