4f91a80e8c9aa1348c214de27eb2c270eca6d9c5
Joshua Portway <josh@stain.org>
2024-12-15 12:20:07 +0000

    Day 13 A better

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -2,41 +2,71 @@ import stain.AbstractPosition
 import stain.Direction
 import stain.Position
 
+import scala.annotation.targetName
 import scala.util.matching.Regex
 
 object Day13 {
 
-  case class Problem(buttonA: Direction, buttonB: Direction, target: AbstractPosition)
+  case class LongVec(stepX:Long, stepY:Long) {
+    @targetName("mult")
+    def *(scalar:Long): LongVec = LongVec(stepX*scalar,stepY*scalar)
+
+    @targetName("sub")
+    def -(other:LongVec): LongVec = LongVec(stepX-other.stepX,stepY-other.stepY)
+
+    @targetName("scalarDiv")
+    def /(scalar:Long): LongVec = LongVec(stepX/scalar,stepY/scalar)
+
+    @targetName("div")
+    def /(other:LongVec):Long = {
+      val xDiv = stepX/other.stepX
+      val yDiv = stepY/other.stepY
+      Math.min(xDiv,yDiv)
+    }
+
+    def modZero(other:LongVec):Boolean = {
+      (stepX % other.stepX == 0) & (stepY % other.stepY == 0)
+    }
+  }
+
+  case class Problem(buttonA: LongVec, buttonB: LongVec, target: LongVec)
 
   @main
   def day13Main(): Unit = {
-    val file  = os.read(os.pwd / "input" / "day13.txt")
+    val file  = os.read(os.pwd / "input" / "day13test.txt")
     val regex = "Button A\\: X\\+(\\d+), Y\\+(\\d+)\\nButton B\\: X\\+(\\d+), Y\\+(\\d+)\\nPrize: X=(\\d+), Y=(\\d+)".r
     val problems = regex
       .findAllMatchIn(file)
       .map { m =>
-        val buttonA = Direction(m.group(1).toInt, m.group(2).toInt)
-        val buttonB = Direction(m.group(3).toInt, m.group(4).toInt)
-        val target  = AbstractPosition(m.group(5).toInt, m.group(6).toInt)
+        val buttonA = LongVec(m.group(1).toLong, m.group(2).toLong)
+        val buttonB = LongVec(m.group(3).toLong, m.group(4).toLong)
+        val target  = LongVec(m.group(5).toInt, m.group(6).toInt)
         Problem(buttonA, buttonB, target)
       }
       .toList
 
-    // why not just brute force it ?
-    val bestSolutions = problems.map { problem =>
-      val solutions = for {
-        aPress <- 0 to 100
-        bPress <- 0 to 100
-        total   = AbstractPosition(0, 0) + ((problem.buttonA * aPress) + (problem.buttonB * bPress))
-        if total == problem.target
-        cost = aPress * 3 + bPress
-      } yield (aPress, bPress, cost)
-      if solutions.isEmpty then None else Some(solutions.minBy((_, _, cost) => cost))
+    // (x * A)+( y * B) = T
+    // (y*B) = T-(x*A)
+    // y = (T-(x*A))/B
+    def solve(problem:Problem): Seq[(Long, Long)] = {
+      val target = problem.target
+      val a = problem.buttonA
+      val b = problem.buttonB
+      val xStep = (target / a)
+      for{
+        x <- 0L to xStep
+        remaining = target-(a * x)
+        if remaining.modZero(b)
+        y = remaining / b
+      } yield (x,y)
     }
-    println(bestSolutions)
-    val onlyValid = bestSolutions.collect { case Some((aPress, bPress, cost)) => cost }
-    val totalCost = onlyValid.sum
-    println(totalCost)
+
+    problems.foreach{ problem =>
+      println(solve(problem))
+    }
+
+//    val partBProblems = problems.map(p => p.copy(target = Position(p.target.x + 10000000000000, p.target.y + 10000000000000)))
+
   }
 
 }
