d958859559c39c742b3a0514c828004f24eb6243
Joshua Portway <josh@stain.org>
2025-01-06 16:57:05 +0000

    Day 19 B

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -2,6 +2,7 @@ import com.barrybecker4.search.AStarSearch
 import com.barrybecker4.search.space.AbstractSearchSpace
 import stain.AllPaths
 
+import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.concurrent.{Await, Future}
 
@@ -9,7 +10,6 @@ object Day19 {
 
   type Towel   = String
   type Pattern = String
-  type Path    = Vector[Towel]
 
   def parse(in: Seq[String]): (Set[Towel], List[Pattern]) = {
     val regex    = """([A-z]+)""".r
@@ -43,65 +43,65 @@ object Day19 {
     val results = patterns.map { pattern =>
       val space  = SearchSpace("", pattern, towels)
       val search = new AStarSearch(space)
-      search.solve
+      val result = search.solve
+      if (result.isEmpty) println(s"$pattern - impossible")
+      result
     }
 
     println(s"part A : ${results.count(_.nonEmpty)}")
 
-    given ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-
-    val work = Future.traverse(patterns) { goal =>
-      Future(countAllPaths(goal, towels.toList))
+    val graph = patterns.map { pattern =>
+      val graph = patternToDag(pattern, towels)
+      val paths = countPaths(graph, pattern)
+      println(s"pattern : $pattern  - count :$paths")
+      paths
     }
+    println(s"part B :${graph.sum}")
+  }
 
-    val pathCounts: Seq[Int] = Await.result(work, scala.concurrent.duration.Duration.Inf)
+  // we can treat all the possible paths through the pattern as a directed graph.
+  // where there are two possible ways of proceeding from a particular state in the pattern
+  // it will be a branch in the graph. Then we can sum all the possible branches through the
+  // graph. We can do this by just recursively counting all paths
+  // it turns out there are a *lot* of paths, so we have to cache the path counts - this will save
+  // us recalculating all of the later branches for every early branch, which should make
+  // the calculation manageable i think
 
-    println(s"Part B : total possible paths : ${pathCounts.sum}")
+  trait DagNode[T] {
+
+    def id: T
+    def children: Set[DagNode[T]]
 
   }
 
-  extension (path: Path) {
+  def countPaths[T](root: DagNode[T], goal: T): Long = {
+    val cache = scala.collection.mutable.Map.empty[T, Long] // mutable is ok. really.
 
-    inline def matches(pattern: Pattern): Boolean =
-      toPattern == pattern
-
-    inline def toPattern = path.mkString
-
-    inline def findPossibleSteps(goal: Pattern, towels: List[Towel]): List[Path] = {
-      val remaining = goal.drop(path.toPattern.length)
-      val nextSteps = towels.filter(remaining.startsWith)
-      nextSteps.map(towel => path.appended(towel)).toList
+    def recurse(n: DagNode[T]): Long = {
+      cache.getOrElseUpdate( // is getOrElseUpdate ok with recursive calls ??
+        n.id,
+        if n.id == goal then 1L
+        else if n.children.isEmpty then 0L
+        else n.children.toList.map(recurse).sum
+        // don't think it'll matter that this isn't tail recursive,
+        // since the maximum depth will be the number of towels in a single pattern
+      )
     }
-
+    recurse(root)
   }
 
-  // depth first search of all possible paths.
-  // at each step we make a list of any possible subsequent steps and then follow the first
-  // until it either reaches the goal or we can't make another step, then go back to the
-  // last queued step. We can probably do this recursively because it won't get that deep
-  // but maybe it would be better to use a lazylist for state management instead. Don't think we need
-  // to build an actual graph structure - just a queue of unprocessed branches.
-  private def countAllPaths(goal: String, towels: List[String]) = {
-    val start                = System.currentTimeMillis()
-    val initialPossibilities = List(Vector.empty[Towel])
-    val pathList = LazyList
-      .unfold[Int, List[Path]](initialPossibilities) { queue =>
-        if queue.isEmpty then None
-        else {
-          val currentPath = queue.head
-          val remaining   = queue.tail
-          if (currentPath.matches(goal)) {
-            Some(1, remaining)
-          } else {
-            val nextSteps = currentPath.findPossibleSteps(goal, towels)
-            Some(0, nextSteps ++ remaining)
-          }
-        }
+  // translates the search for patterns into a directed acyclic graph
+  def patternToDag(goal: Pattern, towels: Set[Towel]): DagNode[Pattern] = {
+    case class PatternDagNode(state: String) extends DagNode[Pattern] {
+      def id = state
+
+      lazy val children: Set[DagNode[Pattern]] = {
+        val remaining = goal.drop(state.length)
+        towels.filter(remaining.startsWith).map(towel => PatternDagNode(state + towel))
       }
-    val pathCount = pathList.count(_ == 1)
-    val end       = System.currentTimeMillis()
-    println(s"number of possible paths for $goal = $pathCount - time to calculate = ${end - start}")
-    pathCount
+
+    }
+    PatternDagNode("")
   }
 
 }
